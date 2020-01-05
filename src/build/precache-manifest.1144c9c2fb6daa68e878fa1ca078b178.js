5d55d18bbd80726113944a368b9edf4cc88d57a3
Jennie Watson <jennie.watson@fairfaxmedia.co.nz>
2020-01-06 11:28:36 +1300

    First commit

---
+++ src/build/precache-manifest.1144c9c2fb6daa68e878fa1ca078b178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9e77d7faf1738d334ad3d83b84d764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac30d791ae31b283bfa",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "d21f5862d06af9520955",
+    "url": "/static/js/2.7f0ddacc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7f0ddacc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ac30d791ae31b283bfa",
+    "url": "/static/js/main.15920aa9.chunk.js"
+  },
+  {
+    "revision": "63b7bf68a48a901e853b",
+    "url": "/static/js/runtime-main.39cdd673.js"
+  }
+]);
